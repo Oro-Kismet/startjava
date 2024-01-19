ce604f1cdc51c71733dfb72ff7ca41f67c85d649
Oro-Kismet <oro-kismet@mail.ru>
2024-01-19 15:26:39 +0500

    Create VariableNamesTheme.java

---
+++ VariableNamesTheme.java
@@ -0,0 +1,70 @@
+public class VariableNamesTheme {
+    public static void main(String[] args) {
+       /* Разные переменные (это название блока) */
+       /* цифра */
+       int number = 5;
+       System.out.print("цифра num = ");
+       System.out.print(number);
+       System.out.print("\n");
+       /*сумма чисел */
+       int numberFirst = 3;
+       int numberSecond = 5;
+       int numberSumm;
+       numberSumm = numberFirst + numberSecond;
+       System.out.print("сумма чисел ");
+       System.out.print(numberFirst);
+       System.out.print(" + ");
+       System.out.print(numberSecond);
+       System.out.print(" = ");
+       System.out.print(numberSumm);
+       System.out.print("\n");
+       /* произведение цифр
+        максимальное число
+        количество десятков
+        вес собаки
+        исходное число
+        процент по вкладу
+        (переменная хранит символ) &
+        код ошибки
+        тип сообщения
+        число нулей
+        уникальное число
+        случайное число
+        математическое выражение
+        выбор (чего-либо)
+        счет (в игре)
+        максимальная длина (строки)
+        пункт меню
+        стоимость кофе на вынос
+        дата начала (чего-либо)
+        окончание диапазона
+        полное имя работника месяца
+        заголовок электронной книги
+        размер
+        вместимость (чего-либо)
+        счетчик
+        путь до файла
+        количество чисел в каждой строке
+        
+        boolean-переменные
+        сотни равны?
+        компьютер включен?
+        есть равные цифры?
+        (что-либо) создано?
+        (что-либо) пустое?
+        (что-либо) активное?
+        новый?
+        электронная почта действительная?
+        имеются уникальные строки?
+        
+        Аббревиатуры
+        старый universally unique identifier
+        производитель оперативной памяти
+        емкость жесткого диска
+        протокол передачи гипертекста
+        сокращенный uniform resource locator
+        новый идентификатор клиента
+        кодировка american standard code for information interchange */
+         System.out.println("Write once, run anywhere");
+    }
+}
